11cd21b51293f81dbcfff6fc38f1adf9e103dc80
Dave Holst <daveholst@gmail.com>
2022-07-26 17:41:58 +0800

    added destructuring, used sets to ensure codes are de-duped

--- python/pipeline.py
+++ python/pipeline.py
@@ -11,6 +11,8 @@ def main(product_data, include_tags, exclude_tags):
     """The implementation of the pipeline test."""
 
     # * query builder that takes an array of tags and builds the appropriate JmesPath Query
+    #! BUG - I believe this query is creating some duplicate results. Has been mitigated by using a set rather
+    #! than a list for product codes in the group_results function
     def tags_to_query(tags):
         #! assumption - if no includes are specified, it prints all (unless an exclude is passed)
         if len(tags) == 0:
@@ -36,6 +38,8 @@ def main(product_data, include_tags, exclude_tags):
     # * result sorter that groups product codes with products of the same name
     # * nested loops may not be the most efficent way to this and may not scale well.
     # * this could be an appropriate solution: https://docs.python.org/3/library/collections.html#collections.defaultdict
+    # * rambda alternative - https://toolz.readthedocs.io/en/latest/
+    # * iteration tools - https://docs.python.org/3/library/itertools.html#itertools.groupby
 
     # TODO add some destructuring in here to clean it up. Is that a thing?
     def group_results(results):
@@ -45,24 +49,24 @@ def main(product_data, include_tags, exclude_tags):
             return []
         # loop through search results
         for result in results:
+            result_name, result_code = result
             # if grouped_results is empty just append it as it is the first result
             if (len(grouped_results) == 0):
                 grouped_results.append(
-                    PreferenceMatch(result[0], [result[1]]))
+                    PreferenceMatch(result_name, {result_code}))
             # else check if the product exists in grouped_results
             else:
                 for index, product in enumerate(grouped_results):
                     # it it exists, add it to the products tuple and break the lookup
-                    if grouped_results[index].product_name == result[0]:
-                        grouped_results[index].product_codes.append(result[1])
+                    if grouped_results[index].product_name == result_name:
+                        grouped_results[index].product_codes.add(
+                            result_code)
                         break
-                    # else, add the product tuple to the list
-                    else:
-                        # was that the last try?
-                        if (index + 1 == len(grouped_results)):
-                            # we didn't find the product in grouped_results so we will add it
-                            grouped_results.append(
-                                PreferenceMatch(result[0], [result[1]]))
+                    # we didn't find the product and that was that the last try
+                    if (index + 1 == len(grouped_results)):
+                        # so we will add it
+                        grouped_results.append(
+                            PreferenceMatch(result_name, {result_code}))
 
         return grouped_results
 
