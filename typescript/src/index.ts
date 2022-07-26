9bcf12cc0271c74ccd8f782fe552f090c07f57fc
Dave Holst <daveholst@gmail.com>
2022-07-26 21:54:16 +0800

    find products - filter, sort and shape working

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,10 +1,14 @@
 import { ProductDataDTO } from './Types'
 import { readFile } from 'fs/promises'
+import { findProducts } from './utils'
 
-/* Import via FS as JSON file could get quite large and be a performance issue if coming in via import  */
-const productData = JSON.parse(
-    await readFile('./assets/product_data.json', 'utf-8')
-) as ProductDataDTO[]
+async function main() {
+    /* Import via FS as JSON file could get quite large and be a performance issue if coming in via import  */
+    const productData = JSON.parse(
+        await readFile('./assets/product_data.json', 'utf-8')
+    ) as ProductDataDTO[]
+    /* Log query to console for demonstration purposes  */
+    console.log(findProducts({ products: productData, exclude: ['green'] }))
+}
 
-console.log(productData)
-// const1 productData = JSON.parse(jsonData)
+main()
--- typescript/src/utils/findProducts.ts
+++ typescript/src/utils/findProducts.ts
@@ -1,16 +1,51 @@
 import { ProductColors, ProductDataDTO, ProductName, ProductSizes } from '../Types'
+import { groupBy } from 'rambda'
 
-interface findProductsOptions {
+interface FindProductsOptions {
     products: ProductDataDTO[]
     include?: Array<ProductColors | ProductSizes>
     exclude?: Array<ProductColors | ProductSizes>
 }
 
-export interface foundProduct {
+export interface FoundProduct {
     name: ProductName
     codes: string[]
 }
 
-export function findProducts({ products, include, exclude }: findProductsOptions): foundProduct[] {
-    return []
+export function findProducts({ products, include, exclude }: FindProductsOptions) {
+    // Filter products according to passed Arguments
+    const filteredProducts = products.filter(product => {
+        const includeAll = !include
+        const onIncludeList = product.tags.some(tag =>
+            include?.some(includeTag => tag === includeTag)
+        )
+        const onExcludeList = product.tags.some(tag =>
+            exclude?.some(excludedTag => tag === excludedTag)
+        )
+
+        return (includeAll && !onExcludeList) || (onIncludeList && !onExcludeList)
+    })
+
+    // Build groupByName helper
+    const groupByName = groupBy((product: ProductDataDTO) => product.name)
+
+    // Group Items
+    const groupedProducts = groupByName(filteredProducts)
+
+    // Sanitize data to FoundProduct shape
+    // TODO Refactor to use maps
+    // TODO Potentially sort products alphabetically
+
+    const foundProducts: FoundProduct[] = []
+    for (const product in groupedProducts) {
+        foundProducts.push({
+            name: product as ProductName,
+            codes: groupedProducts[product]
+                .map(product => product.code)
+                .sort()
+                .reverse(),
+        })
+    }
+
+    return foundProducts
 }
